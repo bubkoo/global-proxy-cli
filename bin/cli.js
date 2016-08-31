390430a4ae8730ee689c27fba7213fbb3ff66354
bubkoo <bubkoo.wy@gmail.com>
2016-08-31 13:47:06 +0800

    init

---
+++ bin/cli.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var actions = require('../lib/index');
+
+var argv = require('minimist')(process.argv.slice(2), {
+    alias: {
+        e: 'enable',
+        d: 'disable',
+        h: 'host',
+        p: 'port',
+        t: 'type',
+        s: 'status',
+        H: 'help',
+        V: 'version'
+    },
+    string: [
+        'port',
+        'host',
+        'type'
+    ],
+    boolean: [
+        'enable',
+        'disable',
+        'status',
+        'help',
+        'version'
+    ],
+    default: {
+        e: false,
+        d: false,
+        s: false,
+        t: 'http'
+    },
+    unknown: function () {
+        //return false;
+    }
+});
+
+
+if (argv.help) {
+    return actions.help();
+}
+
+if (argv.version) {
+    return actions.version();
+}
+
+if (argv.enable) {
+
+    actions.enable(argv);
+
+} else if (argv.disable) {
+
+    actions.disable();
+
+} else if (argv.status && !/^win/.test(process.platform)) {
+
+    actions.status();
+
+} else {
+    actions.help();
+}
